f80e6ef4dba4d009635ddacaa679ae2cec77c248
Alberto Araújo <albertoafaj@hotmail.com>
2022-09-21 11:11:28 -0300

    Implement the userLogout method to reset user information and style login form

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -4,10 +4,13 @@ import Input from '../Forms/Input';
 import Button from '../Forms/Button';
 import useForm from '../../Hooks/useForm';
 import { UserContext } from '../../UserContext';
+import Error from '../Helper/Error';
+import styles from './LoginForm.module.css'
+import stylesBtn from '../Forms/Button.module.css'
 
 const LoginForm = () => {
 
-  const {userLogin} = React.useContext(UserContext)
+  const {userLogin, error, loading} = React.useContext(UserContext)
 
 
   const username = useForm();
@@ -23,14 +26,20 @@ const LoginForm = () => {
   }
 
   return (
-    <section>
-      <h1>Login</h1>
-      <form action="" onSubmit={handleSubmit}>
+    <section className='animeLeft'>
+      <h1 className='title'>Login</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <Input label='Usuário' type='text' name='username' {...username} />
         <Input label='Senha' type='password' name='password' {...password} />
-        <Button>Entrar</Button>
+        {loading ? <Button disabled>Carregando...</Button>: <Button>Entrar</Button>}        
+        <Error error={error} /> 
       </form>
-      <Link to='/login/criar'>Cadastro</Link>
+      <Link className={styles.perdeu} to='/login/perdeu'>Perdeu a senha?</Link>
+      <div className={styles.cadastro}>
+        <h2 className={styles.subtitle}>Cadastre-se</h2>
+        <p >Ainda não possui conta? Cadastre-se no site.</p>
+      </div>
+      <Link className={stylesBtn.button} to='/login/criar'>Cadastro</Link>
     </section>
   )
 }
