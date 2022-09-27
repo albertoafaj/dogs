85832364a842214e7dc9dcf1cce28db7bdf01b20
Alberto Araújo <albertoafaj@hotmail.com>
2022-09-27 10:36:23 -0300

    Implement PhotoComments styles

--- src/Components/Photo/PhotoComments.js
+++ src/Components/Photo/PhotoComments.js
@@ -5,10 +5,15 @@ import styles from './PhotoComments.module.css'
 
 const PhotoComments = (props) => {
     const {login} = React.useContext(UserContext);
+    const commentSection = React.useRef(null);
     const [comments,setComments] = React.useState(()=> props.comments)
+    React.useEffect(()=>{
+        commentSection.current.scrollTop = commentSection.current.scrollHeight;
+    },[comments])
+    
     return (
         <>
-            <ul className={styles.comments}>
+            <ul ref={commentSection} className={styles.comments}>
                 {comments.map(comment => <li key={comment.comment_ID}>
                     <b>{comment.comment_author}: </b>
                     <span>{comment.comment_content}</span>
