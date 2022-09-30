8308186abaf23c656873a9563839e7ec6b558de7
Alberto Araújo <albertoafaj@hotmail.com>
2022-09-30 10:10:20 -0300

    Create component footer

--- src/App.js
+++ src/App.js
@@ -1,42 +1,43 @@
-import React from 'react';
-import './App.css'
-import Footer from './Components/Footer';
-import Header from './Components/Header';
-import Home from './Components/Home';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Login from './Components/Login/Login';
-import { UserStorage } from './UserContext'
-import User from './Components/User/User';
-import ProtectedRoute from './Components/Helper/ProtectedRoute';
-import Photo from './Components/Photo/Photo';
-import UserProfile from './Components/User/UserProfile';
-import NotFound from './Components/NotFound';
+import React from "react";
+import "./App.css";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./Components/Login/Login";
+import { UserStorage } from "./UserContext";
+import User from "./Components/User/User";
+import ProtectedRoute from "./Components/Helper/ProtectedRoute";
+import Photo from "./Components/Photo/Photo";
+import UserProfile from "./Components/User/UserProfile";
+import NotFound from "./Components/NotFound";
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <BrowserRouter>
         <UserStorage>
           <Header />
-          <Routes>
-            <Route path='/' element={<Home />}></Route>
-            <Route path='/login/*' element={<Login />}></Route>
-            <Route
-              path='/conta/*'
-              element={
-                <ProtectedRoute>
-                  <User />
-                </ProtectedRoute>
-              }
-              >
-            </Route>
-              <Route path='/foto/:id' element={<Photo />}></Route>
-              <Route path='/perfil/:user' element={<UserProfile />}></Route>
-              <Route path='*' element={<NotFound />} />
-          </Routes>
+          <main className="AppBody">
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/login/*" element={<Login />}></Route>
+              <Route
+                path="/conta/*"
+                element={
+                  <ProtectedRoute>
+                    <User />
+                  </ProtectedRoute>
+                }
+              ></Route>
+              <Route path="/foto/:id" element={<Photo />}></Route>
+              <Route path="/perfil/:user" element={<UserProfile />}></Route>
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </main>
+          <Footer />
         </UserStorage>
       </BrowserRouter>
-      <Footer />
     </div>
   );
 }
